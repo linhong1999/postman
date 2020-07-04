ab9ff989f4cf858c6ee5455d553d1439bdaeaeb9
winklin <winklin@tencent.com>
2020-07-04 21:10:44 +0800

    minor: 修改自定义异常类

--- commonapi/exceptions.py
+++ commonapi/exceptions.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework.views import exception_handler as handler
-from rest_framework.views import Response
 
+from .commonresponse import CommonResponse
 
 def exception_handler(exc, context):
 
@@ -10,8 +10,6 @@ def exception_handler(exc, context):
 
     if response is None:
         print('%s - %s - %s ' % (context['view'], context['request'].method, exc))
-        return Response({
-            'detail': 'server error',
-        }, status=HTTP_500_INTERNAL_SERVER_ERROR,exception=True)
-        # },status='200')
-    return response
+        return CommonResponse(
+            status=HTTP_500_INTERNAL_SERVER_ERROR,
+            result=False, message='server error', exception=True)
